02fd458118c5c0d68f67c2d2ee12daa11db05431
stephdiep <stephanie.diep002@gmail.com>
2021-12-23 15:40:20 -0500

    Initial Commit

---
+++ SweaterShopApp/SweaterShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopAppApp.swift
+//  SweaterShopApp
+//
+//  Created by Stephanie Diep on 2021-12-23.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
